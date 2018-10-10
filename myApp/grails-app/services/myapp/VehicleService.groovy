96de3a4920ec6eaae779b77ecc9caf6cafec6a4a
Jordan Conard <jordan.conard3@gmail.com>
2018-10-10 19:44:36 -0400

    Created Vehicle scaffold controller for section 6.4.

---
+++ myApp/grails-app/services/myapp/VehicleService.groovy
@@ -0,0 +1,18 @@
+package myapp
+
+import grails.gorm.services.Service
+
+@Service(Vehicle)
+interface VehicleService {
+
+    Vehicle get(Serializable id)
+
+    List<Vehicle> list(Map args)
+
+    Long count()
+
+    void delete(Serializable id)
+
+    Vehicle save(Vehicle vehicle)
+
+}
